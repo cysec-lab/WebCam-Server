51545b8d50098cf4f443e26ff5817af8ac0a428b
RyoIHA <ihara@cysec.cs.ritsumei.ac.jp>
2024-02-05 00:24:03 +0900

    Update cp_conf.sh

--- apache_conf/cp_conf.sh
+++ apache_conf/cp_conf.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #設定ファイルを移動
-sudo mv apache2.conf /etc/apache2/apache2.conf
-sudo mv 000-default.conf /etc/apache2/sites-available/000-default.conf
-sudo mv 001-normal.conf /etc/apache2/sites-available/
-sudo mv 002-trap.conf /etc/apache2/sites-available/
+sudo cp apache2.conf /etc/apache2/apache2.conf
+sudo cp 000-default.conf /etc/apache2/sites-available/000-default.conf
+sudo cp 001-normal.conf /etc/apache2/sites-available/
+sudo cp 002-trap.conf /etc/apache2/sites-available/
 
 sudo systemctl restart apache2
 
